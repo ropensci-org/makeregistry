afb74206442264a714baf81b0d2187dd454ba702
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-31 16:30:28 +0100

    current categories

---
+++ inst/scripts/categories.R
@@ -0,0 +1,12 @@
+old_registry <- jsonlite::read_json("https://raw.githubusercontent.com/ropensci/roregistry/gh-pages/registry.json")
+old_registry <- old_registry$packages
+old_info <- tibble::tibble(name = purrr::map_chr(old_registry, "name"),
+                           ropensci_category = purrr::map_chr(old_registry, "ropensci_category"))
+
+packages <- jsonlite::read_json("https://raw.githubusercontent.com/ropensci/roregistry/ex/registry2.json")
+packages <- tibble::tibble(name = purrr::map_chr(packages$package, "name"))
+
+packages <- dplyr::left_join(packages, old_info,
+                             by = "name")
+
+readr::write_csv(packages, "inst/scripts/categories.csv")
