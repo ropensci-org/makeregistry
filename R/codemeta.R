a677291db274b020784798e81949429873da4e64
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-03 10:57:26 -0800

    fix .create_cm if else thing

--- R/codemeta.R
+++ R/codemeta.R
@@ -1,12 +1,11 @@
-.create_cm <- function(pkg, org, old_cm){
-
-  if(!is.null(old_cm)){
+.create_cm <- function(pkg, org, old_cm) {
+  if (!is.null(old_cm)) {
     if(length(old_cm[purrr::map_chr(old_cm, "identifier") ==
                      gsub("repos\\/.*\\/", "", pkg)]) > 0){
       old_entry <- old_cm[purrr::map_chr(old_cm, "identifier") ==
                gsub("repos\\/.*\\/", "", pkg)][[1]]
 
-      if (!file.exists(file.path(pkg, "codemeta.json"))){
+      if (!file.exists(file.path(pkg, "codemeta.json"))) {
         jsonlite::write_json(old_entry, path = file.path(pkg, "codemeta.json"))
         codemeta_written <- TRUE
       }
@@ -14,39 +13,31 @@
       old_entry <- NULL
     }
   } else {
-      old_entry <- NULL
-    }
+    old_entry <- NULL
+  }
 
   info <- try(codemetar::create_codemeta(pkg = pkg, verbose = FALSE,
-                                         force_update = TRUE),
-              silent = TRUE)
+    force_update = TRUE), silent = TRUE)
 
   if (codemeta_written) {
     file.remove(file.path(pkg, "codemeta.json"))
   }
 
-  if(!inherits(info, "try-error")) {
+  if (!inherits(info, "try-error")) {
     # for other repos, the URLs in DESCRIPTION have to be right
-    if(org %in% c("ropensci", "ropenscilabs")){
+    if (org %in% c("ropensci", "ropenscilabs")) {
       info$codeRepository <- paste0("https://github.com/",
-                                    org, "/", info$identifier)
+        org, "/", info$identifier)
     }
-
     return(info)
-  }else{
+  } else {
     print(toupper(pkg))
-
-    if (is.null(old_entry)){
+    if (is.null(old_entry)) {
       return(old_entry)
-      } else {
-        NULL
-      }
-    }else{
+    } else {
       NULL
     }
-
   }
-
 }
 create_cm <- memoise::memoise(.create_cm)
 
@@ -56,8 +47,6 @@ create_cm <- memoise::memoise(.create_cm)
 #'
 #' @return A JSON codemeta
 #' @export
-#'
-#' @examples
 create_codemetas <- function(old_cm = NULL){
   if(!is.null(old_cm)){
     old_cm <- jsonlite::read_json(old_cm)
