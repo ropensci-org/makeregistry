00d655d18a669649151432205ac50ccb2728f568
Scott Chamberlain <myrmecocystus@gmail.com>
2019-12-05 11:54:29 -0800

    #20 add a ruby and sh script for generating a json file with name and url for each repo

---
+++ inst/automation/git_urls.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'faraday'
+require 'json'
+
+puts "making new git urls file: registry_urls.json"
+
+def fetch_gh(org, page, per_page = 100)
+  url = 'https://api.github.com/orgs/%s/repos' % org
+  resp = Faraday.get(url) do |req|
+    req.params['per_page'] = per_page
+    req.params['page'] = page.to_s
+    req.headers['Authorization'] = 'token ' + ENV['GITHUB_PAT']
+    # req.headers['Authorization'] = 'token ' + ENV['GITHUB_PAT_SCOTT']
+  end
+  return resp
+end
+
+# ropensci
+res_ropensci = []
+(1..5).each do |x|
+  res_ropensci << fetch_gh('ropensci', x)
+end
+
+# ropenscilabs
+res_ropenscilabs = []
+(1..3).each do |x|
+  res_ropenscilabs << fetch_gh('ropenscilabs', x)
+end
+
+ex = File.read("exclude_list.txt").split;
+# ex = File.read("/home/ubuntu/exclude_list.txt").split;
+
+# parse JSON and flatten into an array
+allres = [res_ropensci, res_ropenscilabs].flatten.map { |x| JSON.load(x.body) }.flatten;
+
+# pull out name and git html (https) url
+out = []
+allres.each { |repo|
+  out << {"repo_name" => repo["name"], "git_url" => repo["html_url"]} unless repo["archived"] || ex.include?(repo["name"])
+}
+# add other repos (those not in ropensci or ropenscilabs)
+others = [
+  {"repo_name" => 'auk', "git_url" => 'https://github.com/CornellLabofOrnithology/auk'},
+  {"repo_name" => 'genbankr', "git_url" => 'https://github.com/gmbecker/genbankr'},
+  {"repo_name" => 'treeio', "git_url" => 'https://github.com/YuLab-SMU/treeio'}
+]
+out.concat others
+
+# write json file to disk
+File.open('registry_urls.json', 'w') do |f|
+  f.puts(JSON.pretty_generate(out))
+end
---
+++ inst/automation/git_urls.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ruby git_urls.rb
+
+echo "pushing up registry_urls.json"
+# if make registry did not fail, proceed
+if [ $? -eq 0 ]; then
+  # go into roregistry dir, pull any changes
+  echo "pulling any changes in roregistry"
+  cd roregistry
+  git pull
+
+  # copy files into roregistry directory
+  echo "copy registry_urls.json into roregistry"
+  cp ../registry_urls.json .
+
+  # upload new registry files to github
+  echo "pushing changes to github"
+  git commit -am 'registry_urls.json updated'
+  git push
+
+  # cd back to home dir
+  cd ..
+fi
