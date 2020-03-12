17efad1131c40d0e8fab2480dd785ce90bf5f7c6
Scott Chamberlain <myrmecocystus@gmail.com>
2020-03-12 08:03:35 -0700

    #16 change shell scripts for ropensci and ropenscilabs orgs

---
+++ inst/automation/pull_ropensci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+error_exit()
+{
+    echo "$1" 1>&2
+    exit 1
+}
+
+get_repos()
+{
+    for x in 1 2 3 4 5
+    do
+      echo "working on $x loop"
+      url=$(printf 'https://api.github.com/orgs/ropensci/repos?per_page=100&page=%s' $x)
+      curl -s -H "Authorization: token <replace me: GITHUB_PAT_SCOTT>" $url | ruby -e 'ex=File.read("/home/ubuntu/exclude_list.txt").split; require "json"; JSON.load(STDIN.read).each { |repo| puts repo["name"]; %x[git clone --depth 1 #{repo["html_url"]}] unless File.directory?(repo["name"]) || repo["archived"] || ex.include?(repo["name"]) }'
+    done
+}
+
+
+if rm -rf */; then
+    if get_repos; then
+        echo "yay!"
+    else
+        error_exit "for loop didn't work"
+    fi
+else
+    error_exit "Cannot delete folders! Aborting."
+fi
