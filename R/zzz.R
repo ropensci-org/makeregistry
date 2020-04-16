9d9fe11a61dc8eef546d5062ebb693bde16418f3
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-04-16 07:03:53 +0200

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,21 @@
-is_package <- function(path){
-  all(c("DESCRIPTION", "NAMESPACE", "man", "R") %in%
+is_package <- function(path, old_cm){
+  
+  # Package structure?
+  structure <- all(c("DESCRIPTION", "NAMESPACE", "man", "R") %in%
         dir(path))
+  
+  if (structure) {
+    return(TRUE) # package if it has the right structure
+    } else {
+    if (!is.null(old_cm)) {
+    if (
+      length(old_cm[purrr::map_chr(old_cm, "identifier") ==
+      gsub("repos\\/.*\\/", "", path)]) > 0
+    ) {
+      return(TRUE) # or if it is already in the registry
+      }
+    }
+  
+  return(FALSE) # not a package in other cases (no old registry, or no entry in it)
+  
 }
