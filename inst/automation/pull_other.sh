90cad17a895905c2fa55f5f1b8023c1e42f088f3
Scott Chamberlain <myrmecocystus@gmail.com>
2020-09-10 10:06:12 -0700

    try pull other

--- inst/automation/pull_other.sh
+++ inst/automation/pull_other.sh
@@ -2,7 +2,7 @@
 
 rm -rf */
 
-curl https://raw.githubusercontent.com/ropenscilabs/makeregistry/master/inst/automation/not_transferred.txt > not_transferred.txt
+curl https://raw.githubusercontent.com/ropensci-org/makeregistry/master/inst/automation/not_transferred.txt > not_transferred.txt
 mapfile -t arr < not_transferred.txt
 
 for x in "${arr[@]}"
