ac1b53389a227239fbc9894cdeb8966c528fed7b
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-09-10 14:35:18 +0200

    update

--- inst/scripts/update_categories.R
+++ inst/scripts/update_categories.R
@@ -12,14 +12,18 @@ categories <- readr::read_csv(
 library("magrittr")
 
 # current packages
-jsonlite::read_json("https://raw.githubusercontent.com/ropensci/roregistry/gh-pages/registry.json") %>%
-  .[[1]] %>%
-  purrr::map_chr("name") -> packages
+pkgs <- jsonlite::read_json("https://raw.githubusercontent.com/ropensci/roregistry/gh-pages/registry.json") %>%
+  .[[1]]
+
+purrr::map_chr(pkgs, "name") -> packages
+grepl("ropenscilabs", purrr::map_chr(pkgs, "github")) -> ropenscilabs
 
 categories <- dplyr::left_join(
-  tibble::tibble(name = packages),
+  tibble::tibble(name = packages,
+                 ropenscilabs = ropenscilabs),
   categories
 )
+categories <- dplyr::select(categories, name, ropensci_category, ropenscilabs)
 # update csv by hand and save it
 
 # only keep categories information
