d2f1fdf7c824b6365d4f84a597e15150988aae83
Scott Chamberlain <myrmecocystus@gmail.com>
2020-09-10 11:30:08 -0700

    add last_commits files

---
+++ inst/automation/last_commits.R
@@ -0,0 +1,30 @@
+library(gert)
+library(dplyr)
+dirs <- c(
+  "repos/other",
+  "repos/ropensci",
+  "repos/ropenscilabs"
+)
+each_repo <- function(path) {
+  desc_file <- file.path(path, "DESCRIPTION")
+  if (!file.exists(desc_file)) {
+    df <- data.frame(
+      name = NA_character_,
+      date_last_commit = NA_character_,
+      stringsAsFactors = FALSE)
+    return(df)
+  }
+  name <- desc::desc_get_field("Package", file = desc_file)
+  date <- as.character(gert::git_log(max = 1, repo = path)$time)
+  data.frame(name = name, date_last_commit = date,
+    stringsAsFactors = FALSE)
+}
+out <- list()
+for (i in seq_along(dirs)) {
+  children <- list.dirs(dirs[i], recursive=FALSE)
+  out[[i]] <- lapply(children, each_repo)
+}
+df <- na.omit(dplyr::bind_rows(unlist(out, FALSE)))
+
+f <- "last_commits.csv"
+write.csv(df, file = f, row.names = FALSE)
---
+++ inst/automation/last_commits.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "getting last commit dates across all repos"
+Rscript last_commits.R
