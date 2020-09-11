d6c0d1e6ff1329d28364ea22994c475d3650b3c2
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-09-11 21:03:39 +0200

    some fixes

--- tests/testthat/test-create_registry.R
+++ tests/testthat/test-create_registry.R
@@ -14,12 +14,15 @@ test_that("create_registry works", {
     )
 
   options(repos = c(CRAN = "https://cran.rstudio.com"))
+  mockery::stub(where = create_registry,
+                what = "Sys.time",
+                how = structure(1599850577.88488, class = c("POSIXct", "POSIXt")),
+                depth = 3)
 
   makeregistry::create_registry(
     cm = file.path(tmp, "raw_cm.json"),
     outpat = file.path(tmp, "registry.json")
     )
-
   expect_snapshot_file(file.path(tmp, "registry.json"))
 
 
