7df488a960434722b00b318cc952024e3c29206d
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-02-01 16:01:51 +0100

    progress

--- R/clone-repos.R
+++ R/clone-repos.R
@@ -1,4 +1,4 @@
-#' Clone all ropensci packages
+#' Clone all ropensci packages & ¡¡"last_commits.csv"¡¡
 #'
 #' @export
 #'
@@ -7,13 +7,19 @@ clone_repos <- function() {
   dir.create(file.path("repos", "ropenscilabs"), recursive = TRUE)
   dir.create(file.path("repos", "others"), recursive = TRUE)
   packages <- jsonlite::read_json("https://raw.githubusercontent.com/ropensci/roregistry/gh-pages/packages.json")
-  purrr::walk(packages[1:10], clone_repo)
+  commits <- purrr::map_df(packages[1:10], clone_repo)
+  write.csv(commits, file = "last_commits.csv", row.names = FALSE)
 }
 
 clone_repo <- function(repo) {
   withr::local_dir(guess_folder(repo))
   # TODO: use gert when it becomes possible
   system(sprintf("git clone --depth 1 %s", repo[["url"]]))
+
+  tibble::tibble(
+    name = repo[["package"]],
+    date_last_commit = gert::git_log(max = 1, repo = path)
+  )
 }
 
 guess_folder <- function(repo) {
