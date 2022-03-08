162b4a522962bcf7c4541f571c42104ad59038c5
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-03-08 17:26:03 +0100

    fix

--- tests/testthat/test-build_ropensci_packages_json.R
+++ tests/testthat/test-build_ropensci_packages_json.R
@@ -1,5 +1,5 @@
 test_that("get_hosted_packages works", {
-  pkgs <- get_hosted_packages()
+  pkgs <- get_hosted_packages(former = "https://raw.githubusercontent.com/ropensci/roregistry/gh-pages/packages.json")
   expect_true(length(pkgs) > 100)
   expect_snapshot(unique(lapply(pkgs, names)))
 })
