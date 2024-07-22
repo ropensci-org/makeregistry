33e121cf0156d71ee00fd776d239e965fa402fcc
mpadge <mark.padgham@email.com>
2024-07-22 15:26:58 +0200

    add graphql 'get_releases_query' fn

--- R/pkg-versions.R
+++ R/pkg-versions.R
@@ -4,3 +4,44 @@
 registry_pkg_versions <- function () {
 
 }
+
+get_releases_query <- function (org = "ropensci", n = 100, end_cursor = NULL) {
+
+    after_txt <- ""
+    if (!is.null (end_cursor)) {
+        after_txt <- paste0 (", after:\"", end_cursor, "\"")
+    }
+
+    q <- paste0 ("{
+        repositoryOwner(login:\"", org, "\") {
+            ... on Organization {
+                repositories (first: ", n, after_txt, "){
+                    pageInfo {
+                        hasNextPage
+                        endCursor
+                    }
+                    nodes {
+                        name
+                        url
+                        releases(first: 1, orderBy: {field: CREATED_AT, direction: DESC}) {
+                            nodes {
+                                author {
+                                    email
+                                    login
+                                }
+                                name
+                                createdAt
+                                isLatest
+                                isPrerelease
+                                publishedAt
+                                tagName
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }")
+
+    return (q)
+}
